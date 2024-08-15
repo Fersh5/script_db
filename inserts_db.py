f62b05e3019fd5c517aa9c7c15e75975881fe6c2
Fersh5 <dev_fer5@outlook.com>
2024-08-15 00:19:35 -0600

    insert student

--- inserts_db.py
+++ inserts_db.py
@@ -1,5 +1,35 @@
+import random
+from faker import Faker 
+
+fake = Faker()
+
+def create_students(num):
+    sentence_insert = f"INSERT INTO STUDENTS (FIRSTNAME,LASTNAME,AGE,EMAIL)\nVALUES"
+    students = [(fake.first_name(), fake.last_name(), random.randint(17,35), fake.email()) for _ in range(num)]
+    return sentence_insert, students
 
 
-def insert_alumns(num):
-    sentence_insert = f"INSERT INTO STUDENTS (FIRSTNAME,LASTNAME,AGE,EMAIL)\nVALUES", end=' '
+def show_insert_students(sentense, students):
+      print(sentense)
+      for student in students:
+        if student == students[-1]:
+            print(f'{student}')
+        else:
+            print(f'{student},')
+
+
+
+
+
+
+
+
+
+sentence, student_list = create_students(5)
+show_insert_students(sentence, student_list)
+    
+
+
+
+
 
